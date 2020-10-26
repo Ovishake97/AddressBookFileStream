8b2de0ebe5b372014d0b14283f70fbd78cad5add
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-26 21:41:41 +0530

    [Abhishek] add . added to the UC1 branch

---
+++ AddressBookFileStream/AddressBookMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookFileStream
+{
+    public class AddressBookMain
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string state;
+        public string zip;
+        public string phone;
+        public string email;
+
+
+        public AddressBookMain(string firstName, string lastName, string address, string state, string zip, string phone, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.state = state;
+            this.zip = zip;
+            this.phone = phone;
+            this.email = email;
+        }
+        
+    }
+
+}
+
